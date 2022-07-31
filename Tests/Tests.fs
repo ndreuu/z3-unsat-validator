58e13dbcc9ea3acc3e69f023246d0255f1405ffe
Andrew <a.oleyn1kov@outlook.com>
2022-07-31 20:46:26 +0300

    format

--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -11,31 +11,31 @@ let Setup () = ()
 
 [<Test>]
 let Test1 () =
-    let dir =
-      sprintf "%s/samples%s" Environment.CurrentDirectory
-    
-    let start = fun name path -> Validator.run (dir name) (dir path)
+  let dir =
+    sprintf "%s/samples%s" Environment.CurrentDirectory
 
-    let readLines (filePath: string) =
-      seq {
-        use sr = new StreamReader(filePath)
-    
-        while not sr.EndOfStream do
-          yield sr.ReadLine()
-      }
-    
-    let eq =
-      fun x y -> Seq.fold (&&) true (Seq.zip x y |> Seq.map (fun (x', y') -> x' = y'))
-    
-    let compFolders =  
-      fun fst snd ->
-        Directory.GetFiles(dir fst, "*.smt2")
-        |> fun x ->
-             Directory.GetFiles(dir snd, "*.smt2")
-             |> fun y -> Array.fold2 (fun acc x' y' -> acc && eq (readLines x') (readLines y')) true x y
+  let start =
+    fun name path -> Validator.run (dir name) (dir path)
 
-    
-    start "/drop_id/drop_id.smt2" "/drop_id"
+  let readLines (filePath: string) =
+    seq {
+      use sr = new StreamReader(filePath)
 
-    Assert.IsTrue(compFolders "/drop_id" "/drop_id-match")
+      while not sr.EndOfStream do
+        yield sr.ReadLine()
+    }
 
+  let eq =
+    fun x y -> Seq.fold (&&) true (Seq.zip x y |> Seq.map (fun (x', y') -> x' = y'))
+
+  let compFolders =
+    fun fst snd ->
+      Directory.GetFiles(dir fst, "*.smt2")
+      |> fun x ->
+           Directory.GetFiles(dir snd, "*.smt2")
+           |> fun y -> Array.fold2 (fun acc x' y' -> acc && eq (readLines x') (readLines y')) true x y
+
+
+  start "/drop_id/drop_id.smt2" "/drop_id"
+
+  Assert.IsTrue(compFolders "/drop_id" "/drop_id-match")
